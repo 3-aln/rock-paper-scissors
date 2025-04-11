69a043046dfe3e1949177feb0eedfe7e4bd2c42d
Allen Sun <52902629+3-aln@users.noreply.github.com>
2025-04-10 22:27:15 -0400

    Implement getComputerChoice()

--- scripts.js
+++ scripts.js
@@ -5,9 +5,16 @@ function getComputerChoice() {
   // Get a random number between 0 and 3.
   let randomNum = Math.random() * 3;
   console.log(`randomNum: ${randomNum}`);
-  // If number is in [0, 1), choose rock.
-  // If number is in [1, 2), choose paper.
-  // If number is in [2, 3), choose scissors.
+  if (0 <= randomNum && randomNum < 1) {
+    // If number is in [0, 1), choose rock.
+    return "rock";
+  } else if (1 <= randomNum && randomNum < 2) {
+    // If number is in [1, 2), choose paper.
+    return "paper";
+  } else {
+    // If number is in [2, 3), choose scissors.
+    return "scissors";
+  }
 }
 
-getComputerChoice();
+console.log(getComputerChoice());
