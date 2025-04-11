fb86089e7e622a1461349c8f2ed1a8bce96c1e24
Allen Sun <52902629+3-aln@users.noreply.github.com>
2025-04-10 23:10:35 -0400

    Implement playRound() for the case of user entering rock

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-console.log(getComputerChoice());
-
 let humanScore = 0;
 let computerScore = 0;
 
@@ -9,14 +7,28 @@ const computerSelection = getComputerChoice();
 playRound(humanSelection, computerSelection);
 
 function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  console.log(humanChoice);
+  console.log(computerChoice);
 
+  if (humanChoice === computerChoice) {
+    // If user and computer picked the same option, tie.
+    console.log(`Tie! Both you and the computer picked ${humanChoice}.`);
+  } else if (humanChoice === "rock") {
+    // If user picked rock, indicate win against scissors and lose against paper.
+    if (computerChoice === "scissors") {
+      console.log("You win! Rock beats scissors.");
+    } else if (computerChoice === "paper") {
+      console.log("You lose! Paper beats rock.");
+    }
+  }
 }
 
 // Randomly return "rock", "paper", or "scissors".
 function getComputerChoice() {
   // Get a random number between 0 and 3.
   let randomNum = Math.random() * 3;
-  
+
   if (0 <= randomNum && randomNum < 1) {
     // If number is in [0, 1), choose rock.
     return "rock";
